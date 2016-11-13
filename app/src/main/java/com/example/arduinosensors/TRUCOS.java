5245d0cd4572cc8fc02f667755347f9c4531cd48
TeamAlconst <arhy.jatzive@gmail.com>
2016-11-13 08:12:24 -0400

    Inicio de Codigo
    Todos los archivos para editar la aplicacion

---
+++ app/src/main/java/com/example/arduinosensors/TRUCOS.java
@@ -0,0 +1,13 @@
+package com.example.arduinosensors;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TRUCOS extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trucos);
+    }
+}
